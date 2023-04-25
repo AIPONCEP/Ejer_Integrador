394d6db1513a55084c7ba6383a79ab9191f8a159
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-04-25 12:18:32 +0200

    Primer Commit

---
+++ src/postnet/EntidadFinanciera.java
@@ -0,0 +1,23 @@
+package postnet;
+/**
+ * Esta es la clase entidadFinanciera
+ * @author 1dam Ana Isabel Ponce Pérez
+ */
+public enum EntidadFinanciera { 
+    BIRZA, CASTERMARD;
+
+/**
+ * @return EntidadFinanciera devuelve dos valores BIRZA Y CASTERMARD
+ */
+  
+    public static EntidadFinanciera getBIRZA() {
+        return BIRZA;
+    }
+
+    public static EntidadFinanciera getCASTERMARD() {
+        return CASTERMARD;
+    }
+
+}
+
+  
