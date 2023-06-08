23d62c7959c4778ad938dd36085b692aac185119
AIPONCEP <116910941+AIPONCEP@users.noreply.github.com>
2023-06-08 15:41:48 +0100

    2ºCommit

---
+++ JavaApplication4/src/postnet/EntidadBancaria.java
@@ -0,0 +1,11 @@
+package postnet;
+/**
+ * Esta es la clase entidad financiera
+ * @author 1dam Ana Isabel Ponce Pérez
+ * 
+ */
+public enum EntidadBancaria { BIRZA, CASTERMARD;}
+/**
+ * @return EntidadBancaria devuelve dos valores BIRZA Y CASTERMARD
+ */
+  
---
+++ JavaApplication4/src/postnet/Posnet.java
@@ -0,0 +1,76 @@
+package postnet;
+
+/** 
+ *Clase Posnet
+ *@author 1dam Ana Isabel Ponce Pérez
+ *Atributos de clase: RECARGO_POR_CUOTA, MIN_CANT_CUOTAS Y MAX_CANT_CUOTAS
+ */
+ 
+public class Posnet {
+
+    public static final double RECARGO_POR_CUOTA=0.03;
+    public static int MIN_CANT_CUOTAS=1;
+    public static int MAX_CANT_CUOTAS=6;
+    
+ /**
+  * Constructor Ticket
+  * @param tarjeta
+  * @param montoAbonar
+  * @param cantCuotas
+  * @return Si datosValidos devuelve ticket
+  */
+    
+    public Ticket efectuarPago(TarjetaDeCredito tarjeta,double montoAbonar, int cantCuotas){
+        Ticket ticket=null;
+
+        if(datosValidos(tarjeta, montoAbonar, cantCuotas)){     
+            double montoFinal=montoAbonar+montoAbonar*recargoSegunCuotas(cantCuotas); 
+            if(tarjeta.tieneSaldoDisponible(montoFinal)){
+               String nomApe=tarjeta.nombreCompletoDelTitular(); 
+               tarjeta.descontar(montoFinal);
+               double montoPorCuota=montoFinal/cantCuotas;
+               ticket =new Ticket(nomApe, montoFinal, montoPorCuota);
+            }
+        }
+        return ticket;
+    }
+    
+    private double recargoSegunCuotas(int cantCuotas){
+        return(cantCuotas-1)*RECARGO_POR_CUOTA;
+    }
+        
+    private boolean datosValidos(TarjetaDeCredito tarjeta,double monto, int cant){
+    
+        boolean esTarjetaValida = tarjeta!=null;
+        boolean esMontoValido =monto>0;
+        boolean cantCuotasValidas=cant >=MIN_CANT_CUOTAS && cant<= MAX_CANT_CUOTAS;
+        return  esTarjetaValida && esMontoValido && cantCuotasValidas;
+          
+    }
+
+    //---GETS------------------------
+
+    public static double getRECARGO_POR_CUOTA() {
+        return RECARGO_POR_CUOTA;
+    }
+
+    public static int getMIN_CANT_CUOTAS() {
+        return MIN_CANT_CUOTAS;
+    }
+
+    public static int getMAX_CANT_CUOTAS() {
+        return MAX_CANT_CUOTAS;
+    }
+    
+    //---SETS-------------------------    
+    
+    public static void setMIN_CANT_CUOTAS(int MIN_CANT_CUOTAS) {
+        Posnet.MIN_CANT_CUOTAS = MIN_CANT_CUOTAS;
+    }
+
+    public static void setMAX_CANT_CUOTAS(int MAX_CANT_CUOTAS) {
+        Posnet.MAX_CANT_CUOTAS = MAX_CANT_CUOTAS;
+    }
+       
+    
+}
